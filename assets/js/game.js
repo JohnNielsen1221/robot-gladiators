70b17f94e1e449771a224d47031bf292ba6a251c
John Nielsen <johnhayesnielsen@gmail.com>
2020-05-15 10:20:04 -0600

    changed shop options

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,19 +151,19 @@ var startGame = function() {
 var shop = function() {
   // Ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: \'REFILL\', \'UPGRADE\', or \'LEAVE\' to make a choice.'
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE to make a choice.'
   );
+
+  // Converts string into interger
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case 'refill':
-    case 'REFILL':  
+    case 1:  
       playerInfo.refillHealth();
       break;
-    case 'upgrade':
-    case 'UPGRADE':  
+    case 2:  
       playerInfo.upgradeAttack();
       break;
-    case 'leave':
-    case 'LEAVE':
+    case 3:
       window.alert('Leaving the store.');
       
       // Do nothing, so function will end
